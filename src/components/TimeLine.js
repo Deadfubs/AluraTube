81512b74d1eeb3489f8da33a22c3c1de58112544
Fúlvio <deadfubs@gmail.com>
2023-04-27 16:04:29 -0300

    update TimeLine.js

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -37,7 +37,7 @@ export const StyledTimeline = styled.div`
         scroll-snap-align: start;
         span {
           padding-top: 8px;
-          display: flex;
+          display: block;
           padding-right: 24px;
           color: ${({ theme }) => theme.textColorBase || "#222222"};
         }
