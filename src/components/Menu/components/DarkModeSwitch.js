2f2e0798532f6e664a1482298ce17489d50b9b30
Fúlvio <deadfubs@gmail.com>
2023-04-29 15:01:20 -0300

    functional light and dark mode

--- src/components/Menu/components/DarkModeSwitch.js
+++ src/components/Menu/components/DarkModeSwitch.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { ColorModeContext } from "./ColorMode";
 
 const StyledSwitch = styled.div`
   background-color: #333333;
@@ -36,11 +37,13 @@ const StyledSwitch = styled.div`
 `;
 
 export default function DarkModeSwitch() {
+    const contexto = React.useContext(ColorModeContext);
 
     return (
         <StyledSwitch>
             <input id="darkmode" type="checkbox" onChange={() => {
                 console.log("mudou o state");
+                contexto.toggleMode();
             }} />
             <label
                 htmlFor="darkmode"
